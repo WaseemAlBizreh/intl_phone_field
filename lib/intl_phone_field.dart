3574aa766618a446dc1ba15805a51c587d0a4721
WaseemAlBizreh <waseem.bizreh@sheenvalue.com>
2025-10-17 19:35:06 +0300

    - Update: fix flag v2

--- lib/intl_phone_field.dart
+++ lib/intl_phone_field.dart
@@ -461,16 +461,11 @@ class _IntlPhoneFieldState extends State<IntlPhoneField> {
                   const SizedBox(width: 4),
                 ],
                 if (widget.showCountryFlag) ...[
-                  kIsWeb
-                      ? Image.asset(
-                          'assets/flags/${_selectedCountry.code.toLowerCase()}.png',
-                          package: 'intl_phone_field',
-                          width: 32,
-                        )
-                      : Text(
-                          _selectedCountry.flag,
-                          style: const TextStyle(fontSize: 18),
-                        ),
+                  Image.asset(
+                    'assets/flags/${_selectedCountry.code.toLowerCase()}.png',
+                    package: 'intl_phone_field',
+                    width: 32,
+                  ),
                   const SizedBox(width: 8),
                 ],
                 FittedBox(
